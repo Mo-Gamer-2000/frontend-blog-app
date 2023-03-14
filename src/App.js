a24efb319560f5319ea3e29231fa96ec27928785
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-03-14 12:53:36 +0000

    Website layout implemented and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-500 text-5xl'>Hello, Universe!</h1>
+      <HomePage />
     </div>
   );
 }
