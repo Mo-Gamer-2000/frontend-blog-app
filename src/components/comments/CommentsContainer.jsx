ac7f732fb4cd5f304e426f8a0beca2c75ea1d429
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-05-01 13:00:56 +0100

    Functionality to show comments data from backend.

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -4,65 +4,18 @@ import { getCommentsData } from "../../data/comments";
 import Comment from "./comment";
 import CommentForm from "./CommentForm";
 
-const CommentsContainer = ({ className, logginedUserId }) => {
-    const [comments, setComments] = useState([]);
-    const mainComments = comments.filter((comment) => comment.parent === null);
+const CommentsContainer = ({ className, logginedUserId, comments }) => {
     const [affectedComment, setAffectedComment] = useState(null);
 
-    console.log(comments);
-
-    useEffect(() => {
-        (async () => {
-            const commentData = await getCommentsData();
-            setComments(commentData);
-        })();
-    }, []);
-
     const addCommentHandler = (value, parent = null, replyOnUser = null) => {
-        const newComment = {
-            _id: Math.random().toString(),
-            user: {
-                _id: "a",
-                name: "Moeez Abdul",
-            },
-            desc: value,
-            post: "1",
-            parent: parent,
-            replyOnUser: replyOnUser,
-            createdAt: new Date().toISOString(),
-        };
-        setComments((curState) => {
-            return [newComment, ...curState];
-        });
         setAffectedComment(null);
     };
 
     const updateCommentHandler = (value, commentId) => {
-        const updatedComments = comments.map((comment) => {
-            if (comment._id === commentId) {
-                return { ...comment, desc: value };
-            }
-            return comment;
-        });
-        setComments(updatedComments);
         setAffectedComment(null);
     };
 
     const deleteCommentHandler = (commentId) => {
-        const updatedComments = comments.filter((comment) => {
-            return comment._id !== commentId;
-        });
-        setComments(updatedComments);
-    };
-
-    const getRepliesHandler = (commentId) => {
-        return comments
-            .filter((comment) => comment.parent === commentId)
-            .sort((a, b) => {
-                return (
-                    new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-                );
-            });
     };
 
     return (
@@ -72,7 +25,7 @@ const CommentsContainer = ({ className, logginedUserId }) => {
                 formSubmitHanlder={(value) => addCommentHandler(value)}
             />
             <div className="space-y-4 mt-8">
-                {mainComments.map((comment) => (
+                {comments.map((comment) => (
                     <Comment
                         key={comment._id}
                         comment={comment}
@@ -82,7 +35,7 @@ const CommentsContainer = ({ className, logginedUserId }) => {
                         addComment={addCommentHandler}
                         updateComment={updateCommentHandler}
                         deleteComment={deleteCommentHandler}
-                        replies={getRepliesHandler(comment._id)}
+                        replies={comment.replies}
                     />
                 ))}
             </div>
