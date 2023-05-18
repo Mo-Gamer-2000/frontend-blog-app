c86fddbe15cf80e441a58e5c7b82dd892c7d97ed
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-05-18 21:21:59 +0100

    Article Sidebar - Made it responsive & functional

--- src/pages/articleDetail/container/SuggestedPosts.jsx
+++ src/pages/articleDetail/container/SuggestedPosts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { images, stables } from "../../../constants";
 
 const SuggestedPosts = ({ className, header, posts = [], tags }) => {
     return (
@@ -10,10 +11,13 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
             <div className="grid gap-y-5 mt-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-1">
                 {posts.map((item) => (
                     <div key={item._id} className="flex space-x-3 flex-nowrap items-center">
-                        <img className="aspect-square object-cover rounded-lg w-1/5" src={item.image} alt="laptop" />
+                        <img className="aspect-square object-cover rounded-lg w-1/5"
+                            src={item?.image ? stables.UPLOAD_FOLDER_BASE_URL + item?.image : images.samplePostImage} alt={item.title} />
                         <div className="text-sm font-roboto text-dark-hard font-medium">
                             <h3 className="text-sm font-roboto text-dark-hard font-medium md:text-base lg:text-lg">
-                                {item.title}
+                                <Link to={`/blog/${item.slug}`}>
+                                    {item.title}
+                                </Link>
                             </h3>
                             <span className="text-xs opacity-60">
                                 {new Date(item.createdAt).toLocaleDateString("en-UK", {
@@ -29,14 +33,14 @@ const SuggestedPosts = ({ className, header, posts = [], tags }) => {
             <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
                 Tags
             </h2>
-            <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
+            {tags.length === 0 ? <p className="text-slate-500 text-xs mt-2">There is no tags for this post</p> : <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
                 {tags.map((item, index) => (
                     <Link key={index} to="/"
                         className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm">
                         {item}
                     </Link>
                 ))}
-            </div>
+            </div>}
         </div>
     );
 };
