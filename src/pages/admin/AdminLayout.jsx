23656d7d7ac4039ab5dbdec5016aff013c1c3b63
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-06-08 18:51:28 +0100

    Admin Panel layout complete

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
 
 const AdminLayout = () => {
-    return <div className="flex flex-col h-screen lg:flex-row">
-        <Header />
-    </div>;
+    return (
+        <div className="flex flex-col h-screen lg:flex-row">
+            <Header />
+            <main className="bg-[#F9F9F9] flex-1 p-4 lg:p-6">
+                <Outlet />
+            </main>
+        </div>
+    );
 };
 
 export default AdminLayout;
--- src/pages/admin/Components/Header/Header.jsx
+++ src/pages/admin/Components/Header/Header.jsx
@@ -1,43 +1,174 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { images } from "../../../../constants";
-import { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { logout } from "../../../../store/actions/user";
 
-const Header = () => {
-    const [isMenuActive, setIsMenuActive] = useState(false);
-    const toggleMenuHandler = () => {
-        setIsMenuActive((prevState) => !prevState);
+const navItemsInfo = [
+    { name: "Home", type: "link", href: "/" },
+    { name: "Articles", type: "link", href: "/articles" },
+    {
+        name: "Pages",
+        type: "dropdown",
+        items: [
+            { title: "About us", href: "/about" },
+            { title: "Contact us", href: "/contact" },
+        ],
+    },
+    { name: "Pricing", type: "link", href: "/pricing" },
+    { name: "Faq", type: "link", href: "/faq" },
+];
+
+const NavItem = ({ item }) => {
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdownHandler = () => {
+        setDropdown((curState) => {
+            return !curState;
+        });
     };
 
     return (
-        <header className="flex h-fit w-full items-center justify-between p-4">
-            {/* Logo */}
-            <Link to="/">
-                <img src={images.Logo} alt="logo" className="w-16" />
-            </Link>
-            {/* menu burger icon */}
-            <div className="cursor-pointer">
-                {isMenuActive ? (
-                    <AiOutlineClose className="h-6 w-6" onClick={toggleMenuHandler} />
-                ) : (
-                    <AiOutlineMenu className="h-6 w-6" onClick={toggleMenuHandler} />
-                )}
-            </div>
-            {/* sidebar container */}
-            {isMenuActive && (
-                <div className="fixed inset-0">
-                    {/* underlay */}
-                    <div className="fixed inset-0 bg-black opacity-50" onClick={toggleMenuHandler} />
-                    {/* sidebar */}
-                    <div className="fixed top-0 bottom-0 left-0 z-50 w-3/4 overflow-y-auto bg-white p-4">
-                        <Link to="/">
-                            <img src={images.Logo} alt="logo" className="w-16" />
-                        </Link>
-                        <h4></h4>
+        <li className="relative group">
+            {item.type === "link" ? (
+                <>
+                    <Link to={item.href} className="px-4 py-2">
+                        {item.name}
+                    </Link>
+                    <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+                        /
+                    </span>
+                </>
+            ) : (
+                <div className="flex flex-col items-center">
+                    <button
+                        className="px-4 py-2 flex gap-x-1 items-center"
+                        onClick={toggleDropdownHandler}
+                    >
+                        <span>{item.name}</span>
+                        <MdKeyboardArrowDown />
+                    </button>
+                    <div
+                        className={`${dropdown ? "block" : "hidden"
+                            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                    >
+                        <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                            {item.items.map((page, index) => (
+                                <Link
+                                    key={index}
+                                    to={page.href}
+                                    className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                                >
+                                    {page.title}
+                                </Link>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             )}
-        </header>
+        </li>
+    );
+};
+
+const Header = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [navIsVisible, setNavIsVisible] = useState(false);
+    const userState = useSelector((state) => state.user);
+    const [profileDrowpdown, setProfileDrowpdown] = useState(false);
+
+    const navVisibilityHandler = () => {
+        setNavIsVisible((curState) => {
+            return !curState;
+        });
+    };
+
+    const logoutHandler = () => {
+        dispatch(logout());
+    };
+
+    return (
+        <section className="sticky top-0 left-0 right-0 z-50 bg-white">
+            <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+                <Link to="/">
+                    <img className="w-16" src={images.Logo} alt="logo" />
+                </Link>
+                <div className="lg:hidden z-50">
+                    {navIsVisible ? (
+                        <AiOutlineClose
+                            className="w-6 h-6"
+                            onClick={navVisibilityHandler}
+                        />
+                    ) : (
+                        <AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+                    )}
+                </div>
+                <div
+                    className={`${navIsVisible ? "right-0" : "-right-full"
+                        } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+                >
+                    <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+                        {navItemsInfo.map((item) => (
+                            <NavItem key={item.name} item={item} />
+                        ))}
+                    </ul>
+                    {userState.userInfo ? (
+                        <div className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+                            <div className="relative group">
+                                <div className="flex flex-col items-center">
+                                    <button
+                                        className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+                                        onClick={() => setProfileDrowpdown(!profileDrowpdown)}
+                                    >
+                                        <span>Account</span>
+                                        <MdKeyboardArrowDown />
+                                    </button>
+                                    <div
+                                        className={`${profileDrowpdown ? "block" : "hidden"
+                                            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                                    >
+                                        <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                                            {userState?.userInfo?.admin && (
+                                                <button
+                                                    onClick={() => navigate("/admin")}
+                                                    type="button"
+                                                    className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                                                >
+                                                    Admin Dashboard
+                                                </button>
+                                            )}
+                                            <button
+                                                onClick={() => navigate("/profile")}
+                                                type="button"
+                                                className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                                            >
+                                                Profile Page
+                                            </button>
+                                            <button
+                                                onClick={logoutHandler}
+                                                type="button"
+                                                className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                                            >
+                                                Logout
+                                            </button>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <button
+                            onClick={() => navigate("/login")}
+                            className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+                        >
+                            Sign in
+                        </button>
+                    )}
+                </div>
+            </header>
+        </section>
     );
 };
 
